724516201e2e8c497c96835b1bb242049ee2b1a4
matheus-lima92 <lima.eca@gmail.com>
2018-03-08 01:04:22 -0300

    configuring babel's preset for react

--- ex/index.js
+++ ex/index.js
@@ -1,18 +1,25 @@
-import Pessoa from './pessoa' //só é reconhecido pelo browser após Babel
+/* --- Testing ES6 transpiling ---*/
 
+import Pessoa from './pessoa' //só é reconhecido pelo browser após Babel
 const pessoa = new Pessoa('Matheus')
 console.log(pessoa.toString())
 
+
+/* --- Testing spread operator plugin --- */
 const produto = {
 	nome: "Caneta Preta",
 	preco: 1.90,
 	desconto: 0.05
 }
-
 function clone(objeto) {
 	return { ...objeto} //spread operator
 }
-
 const novoProduto = clone(produto);
 novoProduto.nome = "Caneto Azul";
 console.log(produto, novoProduto)
+
+
+/* --- testing React --- */
+import 'react'
+console.log("React ok");
+export default props => (<h1>Olá</h1>)
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 			loader: 'babel-loader',
 			exclude: /node_modules/, //não processará arquivos dentro dessa pasta
 			query: {
-				presets: ['es2015'], //define o que o loader aplicará como preset (tradução)
+				presets: ['es2015', 'react'], //define o que o loader aplicará como preset (tradução). Presets do babel devem ser instalados via npm
 				plugins: ['transform-object-rest-spread']
 			}
 		}]
